2c573a0252352377abbe31e7f7a151e32597fe90
Clément <clementhottotpro@gmail.com>
2023-01-22 18:31:39 +0100

    LandConfiguration

--- src/main/scala/fr/esgi/al/funprog/land/LandConfiguration.scala
+++ src/main/scala/fr/esgi/al/funprog/land/LandConfiguration.scala
@@ -1,3 +1,7 @@
 package fr.esgi.al.funprog.land
 
-case class LandConfiguration( )
+
+import fr.esgi.al.funprog.land.Instructions.Instruction
+import fr.esgi.al.funprog.land.mower.Mower
+
+case class LandConfiguration( width: Int, height: Int, mowers: Map[Mower, List[Instruction] ])
--- src/main/scala/fr/esgi/al/funprog/land/mower/Mower.scala
+++ src/main/scala/fr/esgi/al/funprog/land/mower/Mower.scala
@@ -1,24 +1,25 @@
-//package fr.esgi.al.funprog.land.mower
-//
-//import fr.esgi.al.funprog.land.mower.Orientation.Orientation
-//
-//class Mower(initialPosition: Position, initialOrientation: Orientation) {
-//  private def position: Position = initialPosition
-//  private var orientation: Orientation = initialOrientation
-//
-//  def getPosition: Position = position
-//
-//  def getOrientation: Orientation = orientation
-//
-//  def moveForward(): Unit = {
-//    position = position.moveForward(orientation)
-//  }
-//
-//  def turnLeft(): Unit = {
-//    orientation = Orientation.turnLeft(orientation)
-//  }
-//
-//  def turnRight(): Unit = {
-//    orientation = Orientation.turnRight(orientation)
-//  }
-//}
+package fr.esgi.al.funprog.land.mower
+
+import fr.esgi.al.funprog.land.mower.Orientation.Orientation
+
+class Mower(initialPosition: Position, initialOrientation: Orientation) {
+
+  private val position: Position = initialPosition
+  private val orientation: Orientation = initialOrientation
+
+  def getPosition: Position = position
+
+  def getOrientation: Orientation = orientation
+
+  def moveForward(): Mower = {
+    new Mower(position.moveForward(orientation), orientation)
+  }
+
+  def turnLeft(): Mower = {
+    new Mower(position.moveForward(orientation), orientation)
+  }
+
+  def turnRight(): Mower = {
+    new Mower(position.moveForward(orientation), orientation)
+  }
+}
