ace8a69b38290cc57b5fc3761a770469d4c40f24
Clément <clementhottotpro@gmail.com>
2023-02-09 00:09:12 +0100

    json output indent

--- src/main/scala/fr/esgi/al/funprog/io/Serializable.scala
+++ src/main/scala/fr/esgi/al/funprog/io/Serializable.scala
@@ -1,5 +1,7 @@
 package fr.esgi.al.funprog.io
 
 trait Serializable[A] {
-  def serialize(a: A): String
+
+  def getTab(depth: Int): String = "\t" * depth
+  def serialize(a: A, depth: Int): String
 }
--- src/main/scala/fr/esgi/al/funprog/io/output/file/JsonFileOutput.scala
+++ src/main/scala/fr/esgi/al/funprog/io/output/file/JsonFileOutput.scala
@@ -10,59 +10,47 @@ import fr.esgi.al.funprog.land.mower.{Mower, Orientation, Position}
 
 object JsonSerializable {
     implicit val serializePosition: Serializable[Position] = new Serializable[Position] {
-        def serialize(position: Position): String =
-            s""""point": {
-               |"x": ${position.getX.toString},
-               |"y": ${position.getY.toString}
-               |}""".stripMargin
+        def serialize(position: Position, depth: Int): String =
+            s"""${getTab(depth)}"point": {
+               |${getTab(depth + 1)}"x": ${position.getX.toString},
+               |${getTab(depth + 1)}"y": ${position.getY.toString}
+               |${getTab(depth)}}""".stripMargin
     }
 
     implicit val serializeOrientation: Serializable[Orientation] = new Serializable[Orientation] {
-        def serialize(orientation: Orientation): String = s""""direction": "${Orientation.toChar(orientation).toString}""""
+        def serialize(orientation: Orientation, depth: Int): String = s"""${getTab(depth)}"direction": "${Orientation.toChar(orientation).toString}""""
     }
 
     implicit val serializeMower: Serializable[Mower] = new Serializable[Mower] {
-        def serialize(mower: Mower): String =
-            s"""{${
-                serializePosition.serialize(mower.getPosition) + "," +
-                    "" + serializeOrientation.serialize(mower.getOrientation)
-            }
-      }"""
+        def serialize(mower: Mower, depth: Int): String =
+            s"""{\n${ serializePosition.serialize(mower.getPosition, depth + 1) + ",\n" + serializeOrientation.serialize(mower.getOrientation, depth + 1) + "\n" +getTab(depth) }}"""
     }
 
     implicit val serializeInstruction: Serializable[List[Instruction]] = new Serializable[List[Instruction]] {
-        def serialize(instructions: List[Instruction]): String = s""""instructions": [${instructions.map(instruction => s""""${instruction.toString}"""").mkString(",")}]"""
+        def serialize(instructions: List[Instruction], depth: Int): String = s"""${getTab(depth)}"instructions": [${instructions.map(instruction => s""""${instruction.toString}"""").mkString(",")}]"""
     }
 
     implicit val serializeLand: Serializable[LandConfiguration] = new Serializable[LandConfiguration] {
-        def serialize(landConfiguration: LandConfiguration): String =
+        def serialize(landConfiguration: LandConfiguration, depth: Int): String =
             s"""{
-               |"limite": {
-               |     "x":${landConfiguration.width.toString},
-               |     "y":${landConfiguration.height.toString}
-               |},
-               |"tondeuses": [
-               |     ${landConfiguration.mowers.map(m => "{\"debut\":" + JsonSerializable.serializeMower.serialize(m._1) + "," + JsonSerializable.serializeInstruction.serialize(m._2) + "}").reduceOption((a, b) => a + "," + b).getOrElse("")}
-               |     ]
+                   |"limite": {
+                   |     "x":${landConfiguration.width.toString},
+                   |     "y":${landConfiguration.height.toString}
+                   |},
+                   |"tondeuses": [\n${landConfiguration.mowers.map(m => getTab(depth + 1) + "{\n"+ getTab(depth+1)+"\"debut\": " + JsonSerializable.serializeMower.serialize(m._1, depth + 1) + ",\n" + JsonSerializable.serializeInstruction.serialize(m._2, depth + 1) + "}").reduceOption((a, b) => a + ",\n" + b).getOrElse("")}]
                |}""".stripMargin
     }
 
     implicit val serializeLandResult: Serializable[LandResult] = new Serializable[LandResult] {
-        def serialize(landResult: LandResult): String =
+        def serialize(landResult: LandResult, depth: Int): String =
             s"""{
-               |"limite": {
-               |     "x":${landResult.width.toString},
-               |     "y":${landResult.height.toString}
-               |},
-               |"tondeuses": [
-               |     ${
-                landResult.mowers.map(m => "{" +
-                    "\"debut\":" + JsonSerializable.serializeMower.serialize(m._1) + ",\n" +
-                    "" + JsonSerializable.serializeInstruction.serialize(m._2) + ",\n" +
-                    "\"fin\":" + JsonSerializable.serializeMower.serialize(m._3) +
-                    "\n}\n").reduceOption((a, b) => a + "," + b).getOrElse("")
-            }
-               |     ]
+               |${getTab(depth)}"limite": {
+               |${getTab(depth+1)}"x": ${landResult.width.toString},
+               |${getTab(depth+1)}"y": ${landResult.height.toString}
+               |${getTab(depth)}},
+               |${getTab(depth)}"tondeuses": [
+               |${ landResult.mowers.map(m => getTab(depth + 1) +"{\n"+ getTab(depth + 2 ) +"\"debut\": " + JsonSerializable.serializeMower.serialize(m._1, depth + 2) + ",\n" + JsonSerializable.serializeInstruction.serialize(m._2, depth + 2) + ",\n" + getTab(depth + 2) +"\"fin\": " + JsonSerializable.serializeMower.serialize(m._3, depth + 2) + "\n"+ getTab(depth + 1) +"}").reduceOption((a, b) => a + ",\n" + b).getOrElse("")}
+               |${getTab(depth)}]
                |}""".stripMargin
     }
 }
@@ -74,7 +62,7 @@ case class JsonFileOutput(name: String) extends FileOutput(name) {
 
         //val test = configuration.mowers.map(m => "\"debut\":" +JsonSerializable.serializeMower.serialize(m._1) +"," + JsonSerializable.serializeInstruction.serialize(m._2) ).fold("")((a,b) => a + "{" + b + "},")
 
-        file.write(JsonSerializable.serializeLand.serialize(configuration))
+        file.write(JsonSerializable.serializeLand.serialize(configuration, 0))
         ()
     }
 
@@ -84,7 +72,7 @@ case class JsonFileOutput(name: String) extends FileOutput(name) {
 
         //val test = configuration.mowers.map(m => "\"debut\":" +JsonSerializable.serializeMower.serialize(m._1) +"," + JsonSerializable.serializeInstruction.serialize(m._2) ).fold("")((a,b) => a + "{" + b + "},")
 
-        file.appendLine(JsonSerializable.serializeLandResult.serialize(configuration))
+        file.overwrite(JsonSerializable.serializeLandResult.serialize(configuration, 1))
         ()
     }
 
